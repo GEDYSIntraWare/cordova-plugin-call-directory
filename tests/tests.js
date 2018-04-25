891b697b4ef63c66f0db6adb1a521c3ad437b34f
Niklas Merz <nmerz@gedys-intraware.de>
2018-04-25 08:52:54 +0200

    Add first tests

--- tests/tests.js
+++ tests/tests.js
@@ -7,6 +7,42 @@ exports.defineAutoTests = function() {
       expect(window.CallDirectory).toBeDefined();
     });
   });
+
+  describe("isAvailable", function () {
+    it("isAvailable schould be defined", function () {
+      expect(window.CallDirectory.isAvailable).toBeDefined();
+    });
+
+    it("isAvailable schould return an result or error in callback", function (done) {
+      window.CallDirectory.isAvailable( function (result) {
+        expect(result).toBeDefined()
+        console.log(result);
+        done();
+      }, function(result) {
+        expect(result).toBeDefined();
+        console.log(result);
+        done();
+      });
+    });
+  });
+
+  describe("addIdentification", function () {
+    it("addIdentification schould be defined", function () {
+      expect(window.CallDirectory.addIdentification).toBeDefined();
+    });
+
+    it("addIdentification schould return an string in callback", function (done) {
+      let testData = [{label: "test", number: "1234567"}]
+      window.CallDirectory.addIdentification(testData, function (result) {
+        expect(result).toEqual("Numbers added to queue");
+        done();
+      }, function(result) {
+        expect(result).toBeDefined();
+        console.log(result);
+        done();
+      });
+    });
+  });
 };
 
 exports.defineManualTests = function (contentEl, createActionButton) {
