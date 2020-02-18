1907bca6c20e47c48cee10f9e893cd7c176c865e
Niklas Merz <nmerz@gedys-intraware.de>
2020-02-18 16:47:05 +0100

    Fix delete

--- src/ios/CallDirectory.swift
+++ src/ios/CallDirectory.swift
@@ -8,12 +8,12 @@ let TABLENAME = "CallDirectoryNumbers"
 @objc(CallDirectory) class CallDirectory : CDVPlugin {
     var defaults = UserDefaults(suiteName: GROUP)
     var logEntries = [String]()
-    
+
     @objc(isAvailable:)
     func isAvailable(_ command: CDVInvokedUrlCommand){
         if #available(iOS 10.0, *) {
             CXCallDirectoryManager.sharedInstance.getEnabledStatusForExtension(withIdentifier: EXTENSION, completionHandler: { (status:CXCallDirectoryManager.EnabledStatus, e:Error?) -> Void in
-                
+
                 if e != nil {
                     print(e ?? "Error")
                     let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: e.debugDescription);
@@ -31,47 +31,45 @@ let TABLENAME = "CallDirectoryNumbers"
             self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
         }
     }
-    
+
     @available(iOS 10.0, *)
     @objc(addIdentification:)
     func addIdentification(_ command: CDVInvokedUrlCommand){
         let data  = command.arguments[0] as! [Any];
         runQuery(mode: "add", data: data)
-        
-        self.log("Done adding numbers to CallDirectoryAdd")
+
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Numbers added to queue");
         self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
     }
-    
+
     @available(iOS 10.0, *)
     @objc(removeIdentification:)
     func removeIdentification(_ command: CDVInvokedUrlCommand){
         let data  = command.arguments[0] as! [Any];
         runQuery(mode: "delete", data: data)
-        
-        self.log("Done adding numbers to CallDirectoryDelete")
+
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Numbers added to delete queue");
         self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
     }
-    
+
     @available(iOS 11.0, *)
     @objc(removeAllIdentification:)
     func removeAllIdentification(_ command: CDVInvokedUrlCommand){
         let db = openDb()
-        if sqlite3_exec(db, "DELETE FROM CallDirectory", nil, nil, nil) != SQLITE_OK {
+        if sqlite3_exec(db, "DELETE FROM \(TABLENAME)", nil, nil, nil) != SQLITE_OK {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             self.log("error dropping table: \(errmsg)")
             let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: errmsg);
             self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
         }
         sqlite3_close(db);
-        
+
         self.defaults?.set(true, forKey: "clearAll")
         self.defaults?.synchronize()
-        
+
         reloadExtension(command)
     }
-    
+
     @available(iOS 11.0, *)
     @objc(getAllItems:)
     func getAllItems(_ command: CDVInvokedUrlCommand){
@@ -94,7 +92,7 @@ let TABLENAME = "CallDirectoryNumbers"
                         if queryResultCol4 != nil {
                             delete = String(cString: queryResultCol4!) == "true"
                         }
-                        
+
                         items.append(["number": numberString, "label": label, "added": queryResultCol2, "updated": queryResultCol3, "delete": delete])
                     } else {
                         self.log("Row invalid")
@@ -104,11 +102,11 @@ let TABLENAME = "CallDirectoryNumbers"
             sqlite3_finalize(queryStatement)
         }
         sqlite3_close(db);
-        
+
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: items);
         self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
     }
-    
+
     @objc(getLog:)
     func getLog(_ command: CDVInvokedUrlCommand){
         var logResult: [AnyHashable : Any] = ["plugin" : self.logEntries]
@@ -117,37 +115,37 @@ let TABLENAME = "CallDirectoryNumbers"
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: logResult);
         self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
     }
-    
+
     //Helper functions
     private func openDb() -> OpaquePointer {
         let fileManager = FileManager.default
         let directory = fileManager.containerURL(forSecurityApplicationGroupIdentifier: GROUP)
         let fileURL = directory?.appendingPathComponent("CordovaCallDirectory.sqlite")
         var db: OpaquePointer?
-        
+
         if sqlite3_open(fileURL?.path, &db) != SQLITE_OK {
             self.log("error opening database")
         }
-        
+
         return db!
     }
-    
+
     private func runQuery(mode: String, data: [Any]) {
         let db = openDb()
         if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS \(TABLENAME) (number TEXT PRIMARY KEY, label TEXT, added NUMERIC, updated NUMERIC, remove TEXT)", nil, nil, nil) != SQLITE_OK {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             self.log("error creating table: \(errmsg)")
         }
-        
+
         //creating a statement
         var stmt: OpaquePointer?
-        
+
         //the query
         var queryString = "REPLACE INTO \(TABLENAME) (number, label, added) VALUES (?,?,?)"
         if mode == "deleteAll" || mode == "delete" {
-            queryString = "UPDATE \(TABLENAME) SET remove = true WHERE number = ?"
+            queryString = "UPDATE \(TABLENAME) SET remove = 'true', updated = ? WHERE number = ?"
         }
-        
+
         //preparing the query
         if sqlite3_exec(db, "BEGIN EXCLUSIVE TRANSACTION", nil, nil, nil) != SQLITE_OK{
             let errmsg = String(cString: sqlite3_errmsg(db)!)
@@ -158,31 +156,37 @@ let TABLENAME = "CallDirectoryNumbers"
             self.log("error preparing insert: \(errmsg)")
             return
         }
-        
+
         for item in data {
             let entry = item as? [String: Any];
-            
+
             //binding the parameters
             if mode == "deleteAll" || mode == "delete" {
-                if sqlite3_bind_text(stmt, 1, (entry!["number"] as! NSString).utf8String, -1, nil) != SQLITE_OK{
+                let unixTime = Date().timeIntervalSince1970
+                if sqlite3_bind_double(stmt, 1, unixTime) != SQLITE_OK{
+                    let errmsg = String(cString: sqlite3_errmsg(db)!)
+                    self.log("failure binding added timestamp: \(errmsg)")
+                    continue
+                }
+
+                if sqlite3_bind_text(stmt, 2, (entry!["number"] as! NSString).utf8String, -1, nil) != SQLITE_OK{
                     let errmsg = String(cString: sqlite3_errmsg(db)!)
                     self.log("failure binding where: \(errmsg)")
                     continue
                 }
             } else {
-                
                 if sqlite3_bind_text(stmt, 1, (entry!["number"] as! NSString).utf8String, -1, nil) != SQLITE_OK{
                     let errmsg = String(cString: sqlite3_errmsg(db)!)
                     self.log("failure binding number: \(errmsg)")
                     continue
                 }
-                
+
                 if sqlite3_bind_text(stmt, 2, (entry!["label"] as! NSString).utf8String, -1, nil) != SQLITE_OK{
                     let errmsg = String(cString: sqlite3_errmsg(db)!)
                     self.log("failure binding label: \(errmsg)")
                     continue
                 }
-                
+
                 let unixTime = Date().timeIntervalSince1970
                 if sqlite3_bind_double(stmt, 3, unixTime) != SQLITE_OK{
                     let errmsg = String(cString: sqlite3_errmsg(db)!)
@@ -190,7 +194,7 @@ let TABLENAME = "CallDirectoryNumbers"
                     continue
                 }
             }
-            
+
             //executing the query
             if  sqlite3_step(stmt) != SQLITE_DONE {
                 let errmsg = String(cString: sqlite3_errmsg(db)!)
@@ -199,7 +203,7 @@ let TABLENAME = "CallDirectoryNumbers"
             }
             sqlite3_reset(stmt);
         }
-        
+
         if  sqlite3_exec(db, "COMMIT TRANSACTION", nil, nil, nil) != SQLITE_OK {
             let errmsg = String(cString: sqlite3_errmsg(db)!)
             self.log("commit failed: \(errmsg)")
@@ -207,18 +211,18 @@ let TABLENAME = "CallDirectoryNumbers"
         self.log("PhoneNumbers processed in \(TABLENAME)")
         sqlite3_finalize(stmt)
         sqlite3_close(db);
-        
+
         //Repeat for all table
         if mode.range(of:"All") == nil {
             runQuery(mode: mode + "All", data: data)
         }
     }
-    
+
     @available(iOS 10.0, *)
     @objc(reloadExtension:)
     func reloadExtension(_ command: CDVInvokedUrlCommand) {
         CXCallDirectoryManager.sharedInstance.reloadExtension(withIdentifier: EXTENSION, completionHandler: { (error) -> Void in
-            
+
             if let error = error {
                 self.log(error.localizedDescription)
                 let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription);
@@ -230,12 +234,12 @@ let TABLENAME = "CallDirectoryNumbers"
             }
         })
     }
-    
+
     private func log(_ message: String) {
         self.logEntries.append(message)
         print(message)
     }
-    
+
     override func pluginInitialize() {
         super.pluginInitialize()
         self.logEntries = []
